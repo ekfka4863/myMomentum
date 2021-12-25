35ca3ea89cd92933387e8085821b55fb3e293ace
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-26 04:05:04 +0900

    [Feat]JS: add function - music player play, pause, change music previouse/next, etc.

--- js/src/musicPlayer.js
+++ js/src/musicPlayer.js
@@ -1,10 +1,85 @@
 // musicPlayer.js
 
+// DOM
+const musicPlayer = document.querySelector(".music_player_src");
+const playerFunction = document.querySelector(".player_function");
+const prevBtn = playerFunction.querySelector("button:nth-child(1)");
+const playBtn = playerFunction.querySelector("button:nth-child(2)");
+const nextBtn = playerFunction.querySelector("button:nth-child(3)");
+const playerAlbum =  document.querySelector(".player_album");
 
-<audio class="my_audio" controls preload="none">
-    <source src="audio/my_song.mp3" type="audio/mpeg">
-    <source src="audio/my_song.ogg" type="audio/ogg">
-</audio>
 
-var audio = new Audio('audio_file.mp3');
-audio.play();
+
+// mp3 files
+const musicFiles = [
+  "../multi/audio/GoAllIn.mp3",
+  "../multi/audio/Antifreeze.mp3",
+  "../multi/audio/FOMO.mp3",
+  "../multi/audio/HateYou.mp3",
+  "../multi/audio/Watch.mp3",
+  "../multi/audio/Tired.mp3",
+  "../multi/audio/Square.mp3",
+];
+
+// background player album image
+const albumImg = [
+  "",
+];
+
+
+// function
+function playMusic () {
+  const PLAYING = "playing";
+
+  const pauseBtnImg = playBtn.querySelector("img");
+
+  if (!musicPlayer.classList.contains(PLAYING)) {
+    musicPlayer.classList.add(PLAYING);
+    musicPlayer.play();
+    pauseBtnImg.src = "../multi/img/music_pause_button.png";
+  } else {
+    // music_pause_button.png
+    musicPlayer.classList.remove(PLAYING);
+    pauseBtnImg.src = "../multi/img/music_play_button.png";
+    musicPlayer.pause();
+  }
+}
+
+let streamingMusicNum = 0;
+
+function prevMusic () {
+  // console.log(streamingMusicNum);
+
+  if (streamingMusicNum > 0) {
+    streamingMusicNum--;
+    musicPlayer.src = musicFiles[streamingMusicNum];
+  } else {
+    musicPlayer.src = musicFiles[musicFiles.length - 1];
+    streamingMusicNum = musicFiles.length - 1;
+  } 
+
+  musicPlayer.play();
+}
+
+function nextMusic () {
+  if (streamingMusicNum < 6) {
+    streamingMusicNum++;
+    musicPlayer.src = musicFiles[streamingMusicNum];
+  } else {
+    streamingMusicNum = 0;
+    musicPlayer.src = musicFiles[streamingMusicNum];
+  }
+
+  musicPlayer.play();
+}
+
+
+function spinAlbum() {
+
+}
+
+
+// event 
+playBtn.addEventListener("click", playMusic);
+prevBtn.addEventListener("click", prevMusic);
+nextBtn.addEventListener("click", nextMusic);
